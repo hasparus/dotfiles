be2cdf80c4b9c3c29b9aa9a75d16ada2145b5e43
Piotr Pietrzak <piotr.pietrzak@explaineverything.com>
2017-10-08 11:00:25 +0200

    Modify aliases and funcs.

--- .funcs.sh
+++ .funcs.sh
@@ -1,16 +1,31 @@
+#! usr/bin/env zsh
+
 gitara ()
 {
   git add -A && git commit -m $1 $2 && git push $2
 }
 
-moov_firstDir="~/workspace/player"
-moov_secondDir="~/workspace"
+function cd_up() {
+  case $1 in
+    *[!0-9]*)                                      # if no a number
+      cd $( pwd | sed -r "s|(.*/$1[^/]*/).*|\1|" ) # search dir_name in current path, if found - cd to it
+      ;;                                           # if not found - not cd
+    *)
+      cd $(printf "%0.0s../" $(seq 1 $1));         # cd ../../../../  (N dirs)
+    ;;
+  esac
+}
+alias 'cd..'='cd_up'
 
-moov ()
-{
-  if [ "$1" == ":2" ]; then
-    cp -R "$moov_firstDir/$2" "$moov_secondDir"
-  elif [ "$1" == ":1" ]; then
-    cp "$moov_secondDir/$2" "$moov_firstDir"
+function nn() {
+  if [ "$1" = "jupyter" ]; then
+    ssh -L 8890:localhost:8890 tryglaw -t \
+    ssh -L 8890:localhost:8890 hera -t \
+    ssh -L 8890:localhost:8890 lab110-02.stud.ii -t \
+    source /pio/os/cuda-8.0/set-env.sh
+    return
   fi
+
+  ssh tryglaw -t ssh hera -t ssh lab110-02.stud.ii -t \
+  source /pio/os/cuda-8.0/set-env.sh
 }
